4a6970e54bec23da71ef375f8e3ec76d5dfb321f
abdallahelkady <abdallahelkady06@gmail.com>
2025-08-06 01:38:54 +0300

    change icon and name app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/views/dash_board_view.dart';
+import 'package:flutter_launcher_icons/android.dart';
 
 void main() {
   //  runApp(const ResponsiveDashBoard());
 
   runApp(
-    DevicePreview(enabled: true, builder: (context) => ResponsiveDashBoard()),
+    //DevicePreview(enabled: true, builder: (context) => ResponsiveDashBoard()),
+    ResponsiveDashBoard(),
   );
 }
 
